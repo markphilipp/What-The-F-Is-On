5a0fcf9282fe06747ab6f0e35f0145b352333645
Mark Philipp <mark.philipp@levvel.io>
2018-11-13 16:55:41 -0500

    Add initial copy of model

---
+++ MovieEntities/MovieRating.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace MovieEntities
+{
+    public class MovieRating
+    {
+        public Guid Id { get; set; }
+        public string Slug { get; set; }
+        public string ContentType { get; set; }
+        public string Title { get; set; }
+        public decimal? ImdbRating { get; set; }
+        public decimal? RottenTomatoesRating { get; set; }
+        public bool HasPoster { get; set; }
+        public bool HasBackdrop { get; set; }
+        public DateTime ReleasedOn { get; set; }
+        public string Classification { get; set; }
+        public string[] Sources { get; set; }
+        public bool OnServices { get; set; }
+        public bool OnFree { get; set; }
+        public bool OnRentPurchase { get; set; }
+        public bool Watchlisted { get; set; }
+        public bool Seen { get; set; }
+    }
+}
