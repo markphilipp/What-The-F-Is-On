5905c30ca0543b69a74b371b9c3fef4be95e44bc
Mark Philipp <mark.philipp@levvel.io>
2018-11-13 17:22:30 -0500

    Initial context add

---
+++ MovieEntities/MovieContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace MovieEntities
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options)
+        {}
+
+        public DbSet<MovieRating> Ratings { get; set; }
+    }
+}
