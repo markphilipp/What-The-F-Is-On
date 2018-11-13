f171e8b23326570ad525a54a4086075c047e6408
Mark Philipp <mark.philipp@levvel.io>
2018-11-13 15:19:15 -0500

    Initial add

---
+++ DataCompiler/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Collections.Generic;
+using MovieEntities.Serialization;
+using System.Threading.Tasks;
+
+namespace DataCompiler
+{
+    class Program
+    {
+        const string urlTemplate = @"https://api.reelgood.com/v2/browse/source/netflix?availability=onSources&content_kind=both&hide_seen=false&hide_tracked=false&hide_watchlisted=false&imdb_end={0}&imdb_start={1}&override_user_sources=true&overriding_free=false&overriding_sources=netflix&rt_end=100&rt_start=0&skip=50&sort=0&sources=netflix&take=50&year_end=2018&year_start=1970";
+
+        static void Main(string[] args)
+        {
+            //for (var i = 0; i <= 100; i++)
+            //{
+
+            //}
+
+            var r = GetResult().Result;
+        }
+
+        private static async Task<List<MovieRating>> GetResult()
+        {
+            var client = new HttpClient()
+            {
+                //BaseAddress = new Uri(String.Format(urlTemplate, i * 100, (i + 1) * 100 - 1))
+            };
+            var deserializer = new DataContractJsonSerializer(typeof(List<MovieRating>));
+
+            var url = String.Format(urlTemplate, 101, 1);
+            var stringResult = await client.GetStringAsync(url);
+            var rawResult = client.GetStreamAsync(url);
+            return deserializer.ReadObject(await rawResult) as List<MovieRating>;
+        }
+    }
+}
+
---
+++ MovieEntities/Serialization/MovieRating.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MovieEntities.Serialization
+{
+    [DataContract]
+    public class MovieRating
+    {
+        [DataMember(Name = "id")]
+        public Guid Id { get; set; }
+
+        [DataMember(Name = "slug")]
+        public string Slug { get; set; }
+
+        [DataMember(Name = "content_type")]
+        public string ContentType { get; set; }
+
+        [DataMember(Name = "title")]
+        public string Title { get; set; }
+
+        [DataMember(Name = "imdb_rating")]
+        public decimal ImdbRating { get; set; }
+
+        [DataMember(Name = "rt_critics_rating")]
+        public decimal RottenTomatoesRating { get; set; }
+
+        [DataMember(Name = "has_poster")]
+        public bool HasPoster { get; set; }
+
+        [DataMember(Name = "has_backdrop")]
+        public bool HasBackdrop { get; set; }
+
+        [DataMember(Name = "released_on")]
+        public DateTime ReleasedOn { get; set; }
+
+        [DataMember(Name = "classification")]
+        public string Classification { get; set; }
+
+        [DataMember(Name = "sources")]
+        public string[] Sources { get; set; }
+
+        [DataMember(Name = "on_services")]
+        public bool OnServices { get; set; }
+
+        [DataMember(Name = "on_free")]
+        public bool OnFree { get; set; }
+
+        [DataMember(Name = "on_rent_purchase")]
+        public bool OnRentPurchase { get; set; }
+
+        [DataMember(Name = "watchlisted")]
+        public bool Watchlisted { get; set; }
+
+        [DataMember(Name = "seen")]
+        public bool Seen { get; set; }
+    }
+}
