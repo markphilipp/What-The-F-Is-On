8f0bfe56de5d159c109eb19ea9cd5aeb72d2690f
Mark Philipp <mark.philipp@levvel.io>
2018-11-14 10:52:23 -0500

    Move Startup

---
+++ MovieEntities/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using MovieEntities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore;
+
+namespace MovieEntities
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Trying here too
+            services.AddDbContext<MovieContext>(options => options.UseSqlite("Data Source=Movie.db", b => b.MigrationsAssembly("MovieEntities")));
+            services.BuildServiceProvider();
+        }
+
+        public void Configure(IApplicationLifetime app)
+        {
+
+        }
+    }
+}
