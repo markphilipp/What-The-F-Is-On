cbf06036f40fd159469ca7aead0e49052a74411b
Mark Philipp <mark.philipp@levvel.io>
2018-11-16 16:55:23 -0500

    Fix loading into models and saving

--- DataCompiler/Services/DataLoader.cs
+++ DataCompiler/Services/DataLoader.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using AutoMapper;
 using DataCompiler.Interfaces;
+using MovieEntities;
 using MovieEntities.Models;
 using Newtonsoft.Json;
 
@@ -14,10 +16,12 @@ namespace DataCompiler.Services
     {
         const string UrlTemplate = @"https://api.reelgood.com/v2/browse/source/netflix?availability=onSources&content_kind=both&hide_seen=false&hide_tracked=false&hide_watchlisted=false&imdb_start={1}&imdb_end={0}&override_user_sources=true&overriding_free=false&overriding_sources=netflix&rt_end=100&rt_start=0&skip=50&sort=0&sources=netflix&take=50&year_end=2018&year_start=1970";
         private readonly IMapper _mapper;
+        private readonly MovieContext _context;
 
-        public DataLoader(IMapper mapper)
+        public DataLoader(IMapper mapper, MovieContext context)
         {
             _mapper = mapper;
+            _context = context;
         }
 
         public void Run()
@@ -26,7 +30,22 @@ namespace DataCompiler.Services
             var rawResults = GetResult().Result;
 
             var models = _mapper.Map<List<MovieRating>>(rawResults);
-            System.Diagnostics.Debugger.Log(1, "Debug", models.ToString());
+
+            var cleanedModels = new List<MovieRating>();
+            foreach (var model in models)
+                cleanedModels.Add(CleanModelSources(model));
+
+            _context.AddRange(cleanedModels);
+            _context.SaveChanges();
+        }
+
+        private static MovieRating CleanModelSources(MovieRating model)
+        {
+            var sourceList = model.Sources.ToList();
+            sourceList.RemoveAll(s => s == null);
+            model.Sources = sourceList;
+
+            return model;
         }
 
         private static async Task<List<MovieEntities.Serialization.MovieRating>> GetResult()
