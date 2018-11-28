2e7c1466436914e3366ca7184a43de7be8d5856b
Mark Philipp <mark.philipp@levvel.io>
2018-11-28 14:24:46 -0500

    Fix repository methods

--- MovieEntities/Repository/IRepository.cs
+++ MovieEntities/Repository/IRepository.cs
@@ -1,6 +1,47 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
 namespace MovieEntities.Repository
 {
     public interface IRepository
     {
+        IEnumerable<T> Set<T>()
+            where T : class;
+
+        T Find<T>(params object[] keys)
+            where T : class;
+
+        Task<T> FindAsync<T>(params object[] keys)
+            where T : class;
+
+        EntityEntry<T> Add<T>(T entity)
+            where T : class;
+
+        Task<EntityEntry<T>> AddAsync<T>(T entity)
+            where T : class;
+
+        void AddRange<T>(IEnumerable<T> entities)
+            where T : class;
+
+        Task AddRangeAsync<T>(IEnumerable<T> entities)
+            where T : class;
+
+        void UpdateRange<T>(IEnumerable<T> entities)
+            where T : class;
+
+        void Remove<T>(T entity)
+            where T : class;
+
+        void RemoveRange<T>(IEnumerable<T> entities)
+            where T : class;
+
+        void Save();
+
+        void SaveAsync();
+
+        DbQuery<T> Query<T>()
+            where T : class;
     }
 }
--- MovieEntities/Repository/Repository.cs
+++ MovieEntities/Repository/Repository.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace MovieEntities.Repository
 {
-    public class Repository<T> : IDisposable, IRepository where T : class
+    public class Repository : IDisposable, IRepository
     {
         private readonly MovieContext _movieContext;
 
@@ -15,52 +15,62 @@ namespace MovieEntities.Repository
             _movieContext = movieContext;
         }
 
-        public IEnumerable<T> Set()
+        public IEnumerable<T> Set<T>()
+            where T : class
         {
             return _movieContext.Set<T>();
         }
 
-        public T Find(params object[] keys)
+        public T Find<T>(params object[] keys)
+            where T : class
         {
             return _movieContext.Find<T>(keys);
         }
 
-        public Task<T> FindAsync(params object[] keys)
+        public Task<T> FindAsync<T>(params object[] keys)
+            where T : class
         {
             return _movieContext.FindAsync<T>(keys);
         }
 
-        public EntityEntry<T> Add(T entity)
+        public EntityEntry<T> Add<T>(T entity)
+            where T : class
         {
             return _movieContext.Add(entity);
         }
 
-        public Task<EntityEntry<T>> AddAsync(T entity)
+        public Task<EntityEntry<T>> AddAsync<T>(T entity)
+            where T : class
         {
             return _movieContext.AddAsync(entity);
         }
 
-        public void AddRange(IEnumerable<T> entities)
+        public void AddRange<T>(IEnumerable<T> entities)
+            where T : class
         {
             _movieContext.AddRange(entities);
         }
 
-        public Task AddRangeAsync(IEnumerable<T> entities)
+        public Task AddRangeAsync<T>(IEnumerable<T> entities)
+            where T : class
         {
             return _movieContext.AddRangeAsync(entities);
         }
 
-        public void UpdateRange(IEnumerable<T> entities)
+        public void UpdateRange<T>(IEnumerable<T> entities)
+            where T : class
         {
             _movieContext.UpdateRange(entities);
         }
 
-        public void Remove(T entity)
+        public void Remove<T>(T entity)
+            where T : class
         {
             _movieContext.Remove(entity);
         }
 
-        public void RemoveRange(IEnumerable<T> entities)
+        public void RemoveRange<T>(IEnumerable<T> entities)
+            where T : class
         {
             _movieContext.RemoveRange(entities);
         }
@@ -75,7 +85,8 @@ namespace MovieEntities.Repository
             _movieContext.SaveChangesAsync();
         }
 
-        public DbQuery<T> Query()
+        public DbQuery<T> Query<T>()
+            where T : class
         {
             return _movieContext.Query<T>();
         }
