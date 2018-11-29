1b897bb99fac0edfb3077ce23776e2c211e7459b
Mark Philipp <mark.philipp@levvel.io>
2018-11-29 18:01:26 -0500

    Add shared registration

---
+++ WhatsOn.ServiceRegistration/ServiceRegistrar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MovieEntities;
+using MovieEntities.Repository;
+
+namespace WhatsOn.ServiceRegistration
+{
+    public class ServiceRegistrar
+    {
+        public void Run(IServiceCollection services)
+        {
+            services.AddDbContext<MovieContext>(options => options.UseSqlite("Data Source=Movie.db", b => b.MigrationsAssembly("MovieEntities")));
+            services.AddScoped<IRepository, Repository>();
+
+            // Initialize automapper
+            services.AddAutoMapper();
+        }
+    }
+}
